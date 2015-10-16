01758bbae0963f2472b3152a097e3a21ec263037
Ubuntu <joakimantus@gmail.com>
2015-10-16 10:17:45 +0000

    updated the script to run as a function instad. Also made sure to remove the old .msh files

--- convert/convert2xml.py
+++ convert/convert2xml.py
@@ -1,21 +1,40 @@
-#!/usr/bin/env python
+#!/usr/bin/python
+###############################################################################
 #
-# Converts one or more msh files to xml files.
-# Using dolfin-convert.py by Anders Logg for file conversion.
-# def gmsh2xml ( ifilename, ofilename ):
+# Name: generate_data.py
+# Arguments (0):
+# 	
+# Output: 
+# Example:
+###############################################################################
 from dolfin_convert import gmsh2xml
-from os import listdir
 import os
+import sys
+
+path = "../naca_airfoil/msh/"
+
+###############################################################################
+# Converts all files form .msh to .xml in the give directory. 
+#	deletes all msh. files when done
+# Arguments (0):
+# 	path: path do directory	
+###############################################################################
+def convert_files_at_path(path):
+	for in_file in os.listdir(path):
+		file_path, file_extension = os.path.splitext(in_file)
+		if file_extension == ".msh":
+			in_file = path+in_file
+			out_file = path+file_path+".xml"
+			if os.path.isfile(out_file):
+				print path+out_file + " already exists!"
+			else:
+				gmsh2xml(in_file, out_file)
+
+			if os.path.isfile(in_file):
+				try:
+					os.remove(in_file)
+				except : 
+					print "Could not remove "+str(in_file)+" due to unexpected error:", sys.exc_info()[0]
 
 
-path = '../naca_airfoil/msh/'
 
-for in_file in listdir(path):
-	file_path, file_extension = os.path.splitext(in_file)
-	if file_extension == ".msh":
-		in_file = path+in_file
-		out_file = path+file_path+".xml"
-		if os.path.isfile(out_file):
-			print path+out_file + " already exists!"
-			continue
-	 	gmsh2xml(in_file, out_file)
