41a4470b763d6c63f6a59a796f37a8c29fd89f3a
Ubuntu <joakimantus@gmail.com>
2015-10-16 08:23:59 +0000

    switch name to lift to drag ratio from parseresults

---
+++ lift_to_drag_ratio.py
@@ -0,0 +1,20 @@
+
+path = "naca_airfoil/navier_stokes_solver/results/"
+filename = "drag_ligt.m"
+
+f = open(path+filename, "r")
+file_content = f.read()
+content_list = file_content.split("\n")
+
+sum_lift = 0
+sum_drag = 0
+
+for line in content_list[1:]:
+	element = line.split("\t")
+	if len(element) < 3:
+		continue
+	sum_lift += float(element[1])
+	sum_drag += float(element[2])
+	
+print sum_drag, sum_lift
+print "lift/drag: ", sum_lift/sum_drag
