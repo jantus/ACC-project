562542e9e0f862e7c1663e380cbd9f4cbb82c455
Ubuntu <joakimantus@gmail.com>
2015-10-18 15:32:00 +0000

    changed so it doesn't delete processed msh files

--- src/convert2xml.py
+++ src/convert2xml.py
@@ -11,7 +11,6 @@ from dolfin_convert import gmsh2xml
 import os
 import sys
 
-path = "../naca_airfoil/msh/"
 
 ###############################################################################
 # Converts all files form .msh to .xml in the give directory. 
@@ -29,10 +28,4 @@ def convert_files_at_path(path):
 				print path+out_file + " already exists!"
 			else:
 				gmsh2xml(in_file, out_file)
-
-			if os.path.isfile(in_file):
-				try:
-					os.remove(in_file)
-				except : 
-					print "Could not remove "+str(in_file)+" due to unexpected error:", sys.exc_info()[0]
 
