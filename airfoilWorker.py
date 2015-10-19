38062479bcf546b61bfaebe8e57feb1a3334d925
oddy555 <oddy46@hotmail.com>
2015-10-19 18:38:24 +0000

    fixed worker

--- airfoilWorker.py
+++ airfoilWorker.py
@@ -9,14 +9,17 @@ os.putenv("LC_ALL", "en_US.UTF-8")
 app = Celery('airfoilWorker',backend='amqp',broke='amqp://')
 
 mshPath = "naca_airfoil/msh/"
-
+currentDir = ""
 @app.task
 def runairfoil(resPath,nSamples,nu,v,t,filename):
+    os.chdir('/home/ubuntu/project/ACC-project')
+    currentDir = os.getcwd()
     out_file = ""
     file_path, file_extension = os.path.splitext(filename)
     if file_extension == ".msh":
-        in_file = mshPath+filename
-        out_file = mshPath+file_path+".xml"
+        in_file = currentDir + '/' + mshPath+filename
+        print in_file
+        out_file = currentDir+'/'+mshPath+file_path+".xml"
         if not(os.path.isfile(out_file)):
             gmsh2xml(in_file, out_file)
         if os.path.isfile(in_file):
@@ -25,18 +28,18 @@ def runairfoil(resPath,nSamples,nu,v,t,filename):
             except : 
                 print "Could not remove "+str(in_file)+" due to unexpected error:", sys.exc_info()[0]
     else:
-        out_file = filename
-    currentDir = os.getcwd()
-    if os.path.isdir(resPath):
-        for file in os.listdir(resPath+'/results'):
-            os.remove(resPath+'/results/'+file)
-        os.rmdir(resPath+'/results')
+        out_file = currentDir+'/'+filename
+    if os.path.isdir(currentDir+'/'+resPath):
+        if os.path.isdir(currentDir+'/'+ resPath+'/results'):
+            for file in os.listdir(resPath+'/results'):
+                os.remove(resPath+'/results/'+file)
+            os.rmdir(resPath+'/results')
         os.rmdir(resPath)
     os.makedirs(resPath)
     os.chdir(currentDir+ '/' + resPath)
     airfoilpath =  './../naca_airfoil/navier_stokes_solver/airfoil'
-    outPath = '../'+out_file    
-    subprocess.call([airfoilpath,nSamples,nu,v,t,outPath],stdout=open(os.devnull,'w'))
+          
+    subprocess.call([airfoilpath,nSamples,nu,v,t,out_file],stdout=open(os.devnull,'w'))
     return
 
 
