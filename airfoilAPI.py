7f4223ccac5acfed75505db2540c0df40e7446c2
oddy555 <oddy46@hotmail.com>
2015-10-19 12:34:33 +0000

    added back flask functionality

--- airfoilAPI.py
+++ airfoilAPI.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask import send_file
 from generate_data import run_script
+from airfoilWorker import runairfoil
 import json
 import os
 from convert2xml import convert_files_at_path
-#fapp = Flask(__name__)
+fapp = Flask(__name__)
 
 
-#@fapp.route('/gen_meshes',methods=['get'])
+@fapp.route('/gen_meshes',methods=['get'])
 def gen_meshes():
     angle_start = str(0)
     angle_stop = str(30)
@@ -17,25 +18,25 @@ def gen_meshes():
     run_script(angle_start,angle_stop,n_angles,n_nodes,n_levels)
     return json.dumps({"gen":"finished"})
 
-#@fapp.route('/run_airfoil',methods=['get'])
+@fapp.route('/run_airfoil',methods=['get'])
 def run_airfoil():
     taskList = []
     n = 0
-    for file in os.listdir('msh'):
-        taskList.append(runairfoil.delay('res'+str(0),'10','0.0001','10','1',str(file)))
+    for file in os.listdir('naca_airfoil/msh'):
+        taskList.append(runairfoil.delay('res'+str(n),'10','0.0001','10','1',str(file)))
         n = n + 1
     for i in range(0,len(taskList)):
-        if taskList[i].ready():
+        while taskList[i].ready():
             continue
-    return jsom.dumps({"airfoil":"finished"})
+    return json.dumps({"airfoil":"finished"})
 
-#@fapp.route('/run',methods=['get'])
+@fapp.route('/run',methods=['get'])
 def run():
     obj = gen_meshes()
     obj2 = run_airfoil()
     return obj2
 
-#if __name__ == '__main__':
-#	fapp.run(host='0.0.0.0',debug=True)
+if __name__ == '__main__':
+	fapp.run(host='0.0.0.0',debug=True)
             
     
