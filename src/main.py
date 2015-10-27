aead067bceecb83bc4f208b89cc2e8aa3181dfaf
Joakim <joakimantus@gmail.com>
2015-10-27 09:56:01 +0000

    Create pngs form result

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import os
 from generate_data import run_script
 from convert2xml import convert_files_at_path as convert
 from generate_data import airfoil
+from plot_result import plot_result
 
 def main():
 	# Go to the right directory
@@ -26,14 +27,22 @@ def main():
 	T = str(1)
 
 	## run airfoul
-	os.chdir("navier_stokes_solver")
 	path = "../msh/"
+	os.chdir("navier_stokes_solver/") 
+	result_list = []
 	for data_file in os.listdir(path):
+		print os.getcwd()
 		file_path, extension = os.path.splitext(data_file) 
 		if extension == ".xml":
 			## start new worker
 			print data_file
 			airfoil(num_samples, visc, speed, T, path+data_file)
-
+			f = open('results/drag_ligt.m', 'r') 
+			result_list.append((data_file, f.read()))		
+	print result_list 
+	os.chdir(cwd)
+	for result in result_list:
+		plot_result(result[0], result[1])
+	
 if __name__ == "__main__":
 	main()
--- src/plot_result.py
+++ src/plot_result.py
@@ -3,11 +3,14 @@
 #
 # Name: plot_file
 # Arguments (0):
-# 	
+#       
 # Output: 
 # Example:
 ###############################################################################
 import os
+import matplotlib
+# Force matplotlib to not use any Xwondows backend.
+matplotlib.use("Agg") 
 import matplotlib.pyplot as plt
 import os.path
 
@@ -15,33 +18,34 @@ import os.path
 # Takes a .m file that is created by the airfoil binary and creates a plot 
 # from the data. Must be in the same directory as the .m file
 # Arguments:
-#	filename: Path o the file
+#       filename: Path o the file
 ###############################################################################
 def plot_file(filename):
-	f = open("drag_ligt.m", "r")
-	s = f.read().split('\n')
-	time = []
-	drag = []
-	lift = []
-	for line in s[1:]:
-		l = line.split(' ')
-		elemlist = []
-		for element in l:
-			if not element == "":
-				elemlist.append(element)
-		time.append(elemlist[0])
-		drag.append(elemlist[1])
-		lift.append(elemlist[2])
+        f = open("drag_ligt.m", "r")
+        s = f.read().split('\n')
+        plot_result(s)
 
-	plt.gca().set_color_cycle(['blue', 'red'])
-	plt.plot(time, drag)
-	plt.plot(time, lift)
-	plt.grid(True)
+def plot_result(filename, result_string):
+    time = []
+    drag = []
+    lift = []
+    data_list = result_string.split('\n')
+    for line in data_list[1:]:
+    	elements = line.split('\t')
+        if len(elements) == 3: 
+	       	time.append(float(elements[0]))
+	        drag.append(float(elements[1]))
+	        lift.append(float(elements[2]))
 
-	plt.legend(['Drag force', 'Lift force'])
-	plt.xlabel('Time')
-	plt.ylabel('Force')
-	plt.title(filename)
-	plt.yscale('log')
-	plt.savefig(os.path.splitext(filename)[0] + '.png')
+    plt.gca().set_color_cycle(['blue', 'red'])
+    plt.plot(time, drag)
+    plt.plot(time, lift)
+    plt.grid(True)
+
+    plt.legend(['Drag force', 'Lift force'])
+    plt.xlabel('Time')
+    plt.ylabel('Force')
+    plt.title(filename)
+    plt.yscale('log')
+    plt.savefig(os.path.splitext(filename)[0] + '.png')
 
