6d740d74873bfb3b77b2ece4c056f6c92c4bb585
Joakim <joakimantus@gmail.com>
2015-10-28 09:53:57 +0000

    Result app

--- src/main.py
+++ src/main.py
@@ -1,9 +1,100 @@
+#!flask/bin/python
+from flask import Flask, jsonify, request, render_template, url_for
 from tasks import work as task
 from plot_result import plot_result
 import worker.server as worker
 import time
+import sys
+import os
 
-def main():
+app = Flask(__name__, template_folder="/home/ubuntu/ACC-project/src")
+@app.route('/')
+def form():
+	return render_template('site/form_submit.html')
+
+@app.route('/runsh/', methods=['POST'])
+def runsh():
+	run_args = {}
+	
+	angle_start = request.form['angle_start']
+	angle_stop = request.form['angle_stop']
+	n_angles = request.form['n_angles']
+	n_nodes = request.form['n_nodes']
+	n_levels = request.form['n_levels']
+
+	num_samples = request.form['num_samples']
+	visc = request.form['visc']
+	speed = request.form['speed']
+	T = request.form['T'] 
+
+	print angle_start
+	print angle_stop
+	print n_angles
+	print n_nodes
+	print n_levels
+		
+	print num_samples
+	print visc
+	print speed
+	print T
+	
+	#############################################################
+	#	Create args
+	#############################################################
+	# run.sh script arguments
+	run_args = {}
+	run_args["angle_start"] = str(0)
+	run_args["angle_stop"] = str(30)
+	run_args["n_angles"] = str(1)
+	run_args["n_nodes"] = str(200)
+	run_args["n_levels"] = str(3)
+
+	# airfoil script argumentes
+	airfoil_args = {}
+	airfoil_args["num_samples"] = str(num_samples)
+	airfoil_args["visc"] = str(visc)
+	airfoil_args["speed"] = str(speed)
+	airfoil_args["T"] = str(T)
+
+	angle_size = int(angle_stop) - int(angle_start) 
+	angle_step = int(angle_size)/int(n_angles)
+	a_list = [angle_step*i for i in range(0, int(n_angles)+1)]
+	args_list = [] 
+	result_list = []
+	for j in range(0, int(n_levels)+1):
+		for i in a_list:
+			filename = "r"+str(j)+"a"+str(i)+"n"+str(n_nodes)
+			if os.path.isfile("plots/"+filename+".png"): 
+				print "Found file: "+filename+".png"
+				plot = filename+".png"
+				result_list.append(plot) 
+				continue
+			else:
+				run_args = {}
+				run_args["angle_start"] = str(angle_start)
+				run_args["angle_stop"] = str(angle_stop)
+				run_args["n_angles"] = str(n_angles)
+				run_args["n_nodes"] = str(n_nodes)
+				run_args["n_levels"] = str(n_levels)
+				
+				args = (filename, run_args, airfoil_args)
+				args_list.append(args)
+
+	for elem in args_list:
+		plot = elem[0]+".png"
+		if os.path.isfile(plot):
+			result_list.append(plot) 
+		else:
+			result_list.append("waiting for "+elem[0]) 
+	
+	return render_template('site/results.html', result=result_list)
+
+@app.route('/results/')
+def results():
+	return render_template('site/results.html')
+
+
+def old():
 		
 	# run.sh script arguments
 	run_args = {}
@@ -21,7 +112,6 @@ def main():
 	airfoil_args["speed"] = str(10)
 	airfoil_args["T"] = str(1)
 
-	print "arguments are set"
 	print run_args
 	print airfoil_args
 	print
@@ -59,6 +149,6 @@ def main():
 	print "creating plots"
 	for result in result_list:
 		plot_result(result[0], result[1])
-	
+
 if __name__ == "__main__":
-	main()
+	app.run(host="0.0.0.0", debug=True )	
